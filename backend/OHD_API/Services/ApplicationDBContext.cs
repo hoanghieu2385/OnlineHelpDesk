bab8cf95ce8d7a890d5b6215110e5954eed6945a
hoanghieu2385 <hoangminhhieu230805@gmail.com>
2024-11-27 11:23:48 +0700

    tạo lại backend

---
+++ backend/OHD_API/Services/ApplicationDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace OHD_API.Services
+{
+    public class ApplicationDBContext : IdentityDbContext
+    {
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
