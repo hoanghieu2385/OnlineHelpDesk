9b12a8a2d544e791af1b8edf25f7fd1cb39af7db
Thanhmai1 <test@w3schools.com>
2024-11-29 23:29:17 +0700

    StatusCrud

--- backend/OHD_API/Services/ApplicationDBContext.cs
+++ backend/OHD_API/Services/ApplicationDBContext.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
-
+using OHD_API.Models;
 namespace OHD_API.Services
 {
     public class ApplicationDBContext : IdentityDbContext
@@ -10,5 +10,6 @@ namespace OHD_API.Services
         public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
         {
         }
+        public DbSet<StatusModel> statusModels { get;}
     }
 }
