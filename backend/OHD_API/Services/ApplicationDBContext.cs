2bb32bd118d89effaf463bcef93c5387fa584195
hoanghieu2385 <hoangminhhieu230805@gmail.com>
2024-12-02 00:40:19 +0700

    x

--- backend/OHD_API/Services/ApplicationDBContext.cs
+++ backend/OHD_API/Services/ApplicationDBContext.cs
@@ -1,23 +1,12 @@
-<<<<<<< HEAD
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-=======
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using OHD_API.Models;
->>>>>>> 1b8b0fcaa8e36160a20d035239584e895dc31639
 namespace OHD_API.Services
 {
     public class ApplicationDBContext : IdentityDbContext
     {
-<<<<<<< HEAD
-        public ApplicationDBContext(DbContextOptions options) : base(options)
-        {
-        }
-=======
         public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
         {
         }
