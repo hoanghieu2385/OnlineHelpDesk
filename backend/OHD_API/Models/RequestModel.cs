fe18f59c6a894c2e745bcd5278cb510bfd40a6fb
lttungog <124686851+lttungog@users.noreply.github.com>
2024-12-01 18:09:53 +0700

    Làm xong Models cho Request và Assignment

--- backend/OHD_API/Models/RequestModel.cs
+++ backend/OHD_API/Models/RequestModel.cs
@@ -1,14 +1,23 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace OHD_API.Models
 {
-    [Table("Requests")]  
+    [Table("Requests")]
     public class RequestModel
     {
-        
+        [Key]
+        [Column("RequestID")]
+        public int RequestID { get; set; }
+        public string UserID { get; set; }
+        public int FacilityID { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
     }
 }
