f3abe4e8c047f221739c89989da85ea87996440e
lttungog <124686851+lttungog@users.noreply.github.com>
2024-12-01 17:59:47 +0700

    Chỉnh Roles - thêm "ASSIGNEE" và bỏ "MANAGER" trong code, thay thế bằng "FACILITY_HEAD"

---
+++ backend/OHD_API/Models/RequestModel.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OHD_API.Models
+{
+    [Table("Requests")]  
+    public class RequestModel
+    {
+        
+    }
+}
