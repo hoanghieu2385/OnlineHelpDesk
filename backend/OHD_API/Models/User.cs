7822e348ee69e0c2ab65326e5c24cebf3426faa7
hoanghieu2385 <hoangminhhieu230805@gmail.com>
2024-12-02 09:30:58 +0700

    merge lại code

--- backend/OHD_API/Models/User.cs
+++ backend/OHD_API/Models/User.cs
@@ -4,12 +4,9 @@ namespace OHD_API.Models
 {
     public class User : IdentityUser
     {
-<<<<<<< HEAD
-=======
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
->>>>>>> 1b8b0fcaa8e36160a20d035239584e895dc31639
     }
 }
